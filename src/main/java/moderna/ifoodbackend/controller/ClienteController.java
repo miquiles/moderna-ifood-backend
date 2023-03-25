2c298bfedb3c1d3292723789e4c8929f8bbc3021
pedromiquiles <miquiles.lima@hotmail.com>
2023-03-25 12:37:23 -0300

    aula03 dada

--- src/main/java/moderna/ifoodbackend/controller/ClienteController.java
+++ src/main/java/moderna/ifoodbackend/controller/ClienteController.java
@@ -1,15 +1,17 @@
 package moderna.ifoodbackend.controller;
 
+import lombok.AllArgsConstructor;
 import moderna.ifoodbackend.model.Cliente;
+import moderna.ifoodbackend.repository.ClienteRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping("/cliente")
+@AllArgsConstructor
 public class ClienteController {
+
+    private ClienteRepository clienteRepository;
 
     @GetMapping("/exibe-texto")
     public String retornaTextoBoasVindas(){
@@ -27,29 +29,10 @@ public class ClienteController {
 
         }
 
-    @GetMapping("/listar-clientes")
-    public List<Cliente> retornaClientes(){
-        Cliente cliente1 = new Cliente();
-        cliente1.setEmail("thebestactor@gmail.com");
-        cliente1.setDataNascimento("12/10/1970");
-        cliente1.setNome("Joey Tribiani");
-
-        Cliente cliente2 = new Cliente();
-        cliente1.setEmail("green@ralphlauren.com");
-        cliente1.setDataNascimento("14/11/1975");
-        cliente1.setNome("Rachel Green");
-
-        Cliente cliente3 = new Cliente();
-        cliente1.setEmail("paleontólogo@gmail.com");
-        cliente1.setDataNascimento("12/10/1972");
-        cliente1.setNome("Ross Geller");
-
-        List listaClientes = new ArrayList();
-        listaClientes.add(cliente1);
-        listaClientes.add(cliente2);
-        listaClientes.add(cliente3);
-
-        return listaClientes;
+    @PostMapping("/salvar-cliente")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente salvarCliente(@RequestBody Cliente cliente){
+        return clienteRepository.save(cliente);
 
     }
 
--- src/main/java/moderna/ifoodbackend/model/Cliente.java
+++ src/main/java/moderna/ifoodbackend/model/Cliente.java
@@ -1,42 +1,32 @@
 package moderna.ifoodbackend.model;
 
-import java.time.LocalDate;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "db_cliente")
 public class Cliente {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(length = 100)
     private String nome;
+
+    @Column(nullable = false)
     private String email;
+
+    @Column(nullable = false)
+    private String documento;
+
     private String dataNascimento; // sub str to LocalDate
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(String dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
 }
